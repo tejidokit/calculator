769fa19f7e1c6a97763304635113662b06d0b5f7
Kit Tejido <kittejido@gmail.com>
2021-03-31 22:00:21 -0600

    created variable for each buttons

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-// alert("hello");
+
+
+// variable for each btn
+
+var oneBtn = document.getElementById('calc-one');
+var oneBtn = document.getElementById('calc-two');
+var oneBtn = document.getElementById('calc-three');
+var oneBtn = document.getElementById('calc-four');
+var oneBtn = document.getElementById('calc-five');
+var oneBtn = document.getElementById('calc-six');
+var oneBtn = document.getElementById('calc-seven');
+var oneBtn = document.getElementById('calc-eight');
+var oneBtn = document.getElementById('calc-nine');
+var oneBtn = document.getElementById('calc-zero');
+
+var decimalBtn = document.getElementById('calc-decimal');
+var clearBtn = document.getElementById('calc-clear');
+var backspaceBtn = document.getElementById('calc-backspace');
+var displayValElement = document.getElementById('calc-display-val');
+
+var calcNumBtns = document.getElementsByClassName('calc-btn-num');
+var calcOperatorBtns = documenet.getElementsByClassName('calc-btn-operator);
+
