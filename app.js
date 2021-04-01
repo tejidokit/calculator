2eb0262fb3088033b363bbdd0082e0c7d8f868cb
Kit Tejido <kittejido@gmail.com>
2021-04-01 00:50:20 -0600

    event listeners

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 
-// variable for each btn
+// ### variable for each btn ###
 
 var oneBtn = document.getElementById('calc-one');
 var oneBtn = document.getElementById('calc-two');
@@ -21,3 +21,24 @@ var displayValElement = document.getElementById('calc-display-val');
 var calcNumBtns = document.getElementsByClassName('calc-btn-num');
 var calcOperatorBtns = documenet.getElementsByClassName('calc-btn-operator);
 
+
+// ### create Even Listner for each button  ###
+
+var displayVal = '0';
+var pendingVal;
+var evalStringArray = [];
+
+var updateDisplayVal = (clickObj) => {
+    var btnTxt = clickObj.target.innerText;
+
+
+}
+
+for (let i = 0; i < calcNumBtns.length; i++) {
+    calcNumBtns[i].addEventListener('click', updateDisplayVal, false);
+}
+
+// for (let i = 0; i < calcOperatorBtns.length; i++) {
+//     calcOperatorBtns[i].addEventListener('click', performOperation, false);
+
+// }
